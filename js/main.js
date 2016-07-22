b4f0a24c535dcb75ddc345fcffca17270ab9168d
Levski Weng <levskiweng@gmail.com>
2016-07-23 00:04:39 +0800

    blog structure construction

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+function parseMarkdownTextTitle(text) {
+    var lines = text.match(/^.*([\n\r]+|$)/gm);
+
+    for (i = 0; i < lines.length; i++) {
+        // find the first line begins with '#', it means that it's the title
+        if (lines[i].match(/^#[~#\w\s\t]+/gm)) {
+            return lines[i].replace(/#/g, '').trim();
+        }
+    }
+
+    return "";
+}
+
 function loadBody() {
     var canvas = document.getElementById('background_canvas');
     var context;
@@ -5,4 +18,29 @@ function loadBody() {
 
     $('#blog').css('background', 'url("' + canvas.toDataURL() + '")');
     $('#blog').css('background-repeat', 'repeat-y');
+
+    $('.panel-body a').each(function (){
+        var node = $(this);
+        //alert($(this).attr('href'));
+        $.get( $(this).attr('href'), function( data ) {
+            // Change Markdown parse engine from showdown to markdown-it
+            // because markdown-it has the ability to render table
+            //var converter = new showdown.Converter();
+            //$("#preview").html(converter.makeHtml(data));
+            var md = window.markdownit({
+                html : true,
+                breaks: true
+            });
+            // Enable superscript and subscript feature by markdown-it plugins:
+            // markdown-it-sup and markdown-it-sub
+            window.markdownitSup(md);
+            window.markdownitSub(md);
+            
+            $(node).parent().html(md.render(data));
+        });
+    });
+    // Don't use hljs.initHighlightingOnLoad()
+    // because the markdown document is loaded dynamically
+    hljs.initHighlighting();
+
 }
